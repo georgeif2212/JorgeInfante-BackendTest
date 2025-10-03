cbbcf35c1265ac82734375858c6c53721611b77b
Jorge Infante <jinfante2212@gmail.com>
2025-10-03 13:35:45 -0500

    feat:initialize database before starting server

--- src/server.js
+++ src/server.js
@@ -1,10 +1,16 @@
 import http from "http";
 import config from "./config/config.js";
 import app from "./app.js";
+import { initDB } from "./db/mongodb.js";
 
+// * Creamos el servidor HTTP usando Express
 const server = http.createServer(app);
 const PORT = config.port;
 
+// * Inicializamos la base de datos antes de levantar el servidor
+await initDB();
+
+// * Iniciamos el servidor en el puerto configurado
 server.listen(PORT, () => {
   console.log(`Server running in http://localhost:${PORT} 🚀`);
 });
