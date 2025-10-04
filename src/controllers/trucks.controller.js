9b3c4d577bab269fcc5ebd978c11893fdea82d1a
Jorge Infante <jinfante2212@gmail.com>
2025-10-04 17:16:48 -0500

    feat: static method to create a new truck validating it doesn't exist by using plates and the user exists

--- src/controllers/trucks.controller.js
+++ src/controllers/trucks.controller.js
@@ -1,7 +1,30 @@
+import { CustomError } from "../utils/CustomError.js";
 import TruckModel from "../models/truck.model.js";
+import EnumsError from "../utils/EnumsError.js";
+import messageError from "../utils/ErrorCauseMessage.js";
+import { createPasswordHash } from "../utils/utils.js";
+import UsersController from "./users.controller.js";
 
 export default class TrucksController {
   static get(query = {}) {
     return TruckModel.find(query);
+  }
+
+  static async create(data) {
+    const { plates, user } = data;
+
+    const truck = await TrucksController.get({ plates: plates });
+    if (truck.length > 0) {
+      CustomError.create({
+        name: "Invalid truck data",
+        cause: messageError.generatorTruckAlreadyExistsError(data),
+        message: `Truck already exists`,
+        code: EnumsError.CONFLICT,
+      });
+    }
+
+    await UsersController.getById(user);
+
+    return TruckModel.create(data);
   }
 }
