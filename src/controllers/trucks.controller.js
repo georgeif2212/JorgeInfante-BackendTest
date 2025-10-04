8d156cb31437612200740994fbd5b6cfc96447f3
Jorge Infante <jinfante2212@gmail.com>
2025-10-04 18:25:25 -0500

    docs: trucks controller documentation

--- src/controllers/trucks.controller.js
+++ src/controllers/trucks.controller.js
@@ -1,15 +1,32 @@
+/**
+ * @file trucks.controller.js
+ * @description Controlador para la entidad Truck. Incluye operaciones CRUD
+ *              y manejo de errores personalizado usando CustomError.
+ */
+
 import { CustomError } from "../utils/CustomError.js";
 import TruckModel from "../models/truck.model.js";
 import EnumsError from "../utils/EnumsError.js";
 import messageError from "../utils/ErrorCauseMessage.js";
-import { createPasswordHash } from "../utils/utils.js";
 import UsersController from "./users.controller.js";
 
 export default class TrucksController {
+  /**
+   * Obtener todos los camiones que cumplen con un query opcional.
+   * @param {Object} query - Filtro opcional para la búsqueda.
+   * @returns {Promise<Array>} Lista de camiones.
+   */
   static get(query = {}) {
     return TruckModel.find(query);
   }
 
+  /**
+   * Crear un nuevo camión.
+   * Valida que las placas sean únicas y que el usuario exista.
+   * @param {Object} data - Datos del camión a crear.
+   * @returns {Promise<Object>} Camión creado.
+   * @throws CustomError si ya existe un camión con las mismas placas o usuario inválido.
+   */
   static async create(data) {
     const { plates, user } = data;
 
@@ -28,6 +45,12 @@ export default class TrucksController {
     return TruckModel.create(data);
   }
 
+  /**
+   * Obtener un camión por su ID.
+   * @param {String} tid - ID del camión.
+   * @returns {Promise<Object>} Camión encontrado.
+   * @throws CustomError si no existe el camión.
+   */
   static async getById(tid) {
     const truck = await TruckModel.findById(tid);
     if (!truck) {
@@ -41,6 +64,14 @@ export default class TrucksController {
     return truck;
   }
 
+  /**
+   * Actualizar un camión por su ID.
+   * Valida placas duplicadas y existencia de usuario si se proporciona.
+   * @param {String} tid - ID del camión.
+   * @param {Object} data - Datos a actualizar.
+   * @returns {Promise<Object>} Camión actualizado.
+   * @throws CustomError si el camión no existe o datos inválidos.
+   */
   static async updateById(tid, data) {
     const { plates, user } = data;
 
@@ -73,6 +104,12 @@ export default class TrucksController {
     return updatedTruck;
   }
 
+  /**
+   * Eliminar un camión por su ID.
+   * @param {String} tid - ID del camión.
+   * @returns {Promise<Object>} Camión eliminado.
+   * @throws CustomError si no existe el camión.
+   */
   static async deleteById(tid) {
     const result = await TruckModel.findByIdAndDelete(tid);
     if (!result) {
