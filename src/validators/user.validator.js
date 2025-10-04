6fd42a22d65ae0ecd9b65305e1764f5818dda2f6
Jorge Infante <jinfante2212@gmail.com>
2025-10-04 15:53:12 -0500

    feat: new uid mongodb validator

--- src/validators/user.validator.js
+++ src/validators/user.validator.js
@@ -10,3 +10,14 @@ export const loginSchema = Joi.object({
   email: Joi.string().email().required(),
   password: Joi.string().required(),
 });
+
+export const uidSchema = Joi.object({
+  uid: Joi.string()
+    .pattern(/^[0-9a-fA-F]{24}$/)
+    .required()
+    .messages({
+      "string.pattern.base": "Invalid MongoDB ObjectId format",
+      "string.empty": "User ID cannot be empty",
+      "any.required": "User ID is required",
+    }),
+});
