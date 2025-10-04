8334f123823f0c7d97ce9c537a9a7f5d730e426e
Jorge Infante <jinfante2212@gmail.com>
2025-10-04 14:52:00 -0500

    feat: new error messages for login

--- src/utils/ErrorCauseMessage.js
+++ src/utils/ErrorCauseMessage.js
@@ -3,6 +3,16 @@ const messageError = {
     return `The identifier must be valid - Received ID: ${id}`;
   },
 
+  generatorUserLoginError: (data) => {
+    return `Todos los campos son requeridos
+    - email: ${data.email}
+    - password: ${data.password}`;
+  },
+
+  generatorUserLoginDataError: () => {
+    return `Invalid email or password`;
+  },
+
 };
 
 export default messageError;
