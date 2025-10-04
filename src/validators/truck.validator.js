a77483fac8df0b1f512d228b10cb0682ac0cffd7
Jorge Infante <jinfante2212@gmail.com>
2025-10-04 18:00:40 -0500

    feat: new validator mongo id

--- src/validators/truck.validator.js
+++ src/validators/truck.validator.js
@@ -1,6 +1,18 @@
 import Joi from "joi";
 import { uidSchema } from "./user.validator.js";
 
+export const tidSchema = Joi.object({
+  tid: Joi.string()
+    .pattern(/^[0-9a-fA-F]{24}$/)
+    .required()
+    .messages({
+      "string.pattern.base": "Invalid MongoDB ObjectId format",
+      "string.empty": "Truck ID cannot be empty",
+      "any.required": "Truck ID is required",
+    }),
+});
+
+
 export const truckSchema = Joi.object({
   year: Joi.string().required(),
   color: Joi.string().required(),
