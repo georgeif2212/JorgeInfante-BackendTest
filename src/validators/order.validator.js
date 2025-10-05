c1ac56d4f6d7ee212b3aa0d8875f886a89bce39c
Jorge Infante <jinfante2212@gmail.com>
2025-10-05 13:12:08 -0500

    feat: new validators schemas for updating and id

--- src/validators/order.validator.js
+++ src/validators/order.validator.js
@@ -1,7 +1,7 @@
 import Joi from "joi";
-import { uidSchema } from "./user.validator.js";
-import { tidSchema as truckIdSchema } from "./truck.validator.js";
-import { lidSchema as locationIdSchema } from "./location.validator.js";
+import { uidSchema } from "./user.validator.js"; // para validar ObjectId
+import { tidSchema as truckIdSchema } from "./truck.validator.js"; // si tienes algo similar
+import { lidSchema as locationIdSchema } from "./location.validator.js"; // idem
 
 // Crear una orden
 export const orderSchema = Joi.object({
@@ -12,15 +12,30 @@ export const orderSchema = Joi.object({
   dropoff: locationIdSchema.extract("lid").required(), // destination location
 });
 
-// // Actualizar una orden (todos opcionales pero se requiere al menos uno)
-// export const updateOrderSchema = Joi.object({
-//   user: uidSchema.extract("uid").optional(),
-//   truck: truckIdSchema.extract("uid").optional(),
-//   status: Joi.string().valid("created", "in transit", "completed").optional(),
-//   pickup: locationIdSchema.extract("uid").optional(),
-//   dropoff: locationIdSchema.extract("uid").optional(),
-// })
-//   .min(1)
-//   .messages({
-//     "object.min": "At least one field must be provided to update the order",
-//   });
+// Actualizar una orden (todos opcionales pero se requiere al menos uno)
+export const updateOrderSchema = Joi.object({
+  user: uidSchema.extract("uid").optional(),
+  truck: truckIdSchema.extract("tid").optional(),
+  status: Joi.string().valid("created", "in transit", "completed").optional(),
+  pickup: locationIdSchema.extract("lid").optional(),
+  dropoff: locationIdSchema.extract("lid").optional(),
+})
+  .min(1)
+  .messages({
+    "object.min": "At least one field must be provided to update the order",
+  });
+
+/**
+ * Esquema de validación para el parámetro `oid`
+ * Valida que cumpla con el formato de un ObjectId de MongoDB
+ */
+export const oidSchema = Joi.object({
+  oid: Joi.string()
+    .pattern(/^[0-9a-fA-F]{24}$/)
+    .required()
+    .messages({
+      "string.pattern.base": "Invalid MongoDB ObjectId format",
+      "string.empty": "Location ID cannot be empty",
+      "any.required": "Location ID is required",
+    }),
+});
