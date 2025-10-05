042a899d99e26dd02614f57e4cd341c53b06f2ba
Jorge Infante <jinfante2212@gmail.com>
2025-10-04 23:25:29 -0500

    feat: static method which creates a new order but validating user, truck and locations exists

--- src/controllers/orders.controller.js
+++ src/controllers/orders.controller.js
@@ -5,8 +5,11 @@
  */
 
 import OrderModel from "../models/order.model.js";
+import LocationsController from "./locations.controller.js";
+import TrucksController from "./trucks.controller.js";
+import UsersController from "./users.controller.js";
 
-export default class LocationsController {
+export default class OrdersController {
   /**
    * Obtener todas las ordenes que cumplen con un query opcional.
    * @param {Object} query - Filtro opcional para la búsqueda.
@@ -14,5 +17,27 @@ export default class LocationsController {
    */
   static get(query = {}) {
     return OrderModel.find(query);
+  }
+
+  /**
+   * Crear un nuevo camión.
+   * Valida que las placas sean únicas y que el usuario exista.
+   * @param {Object} data - Datos del camión a crear.
+   * @returns {Promise<Object>} Camión creado.
+   * @throws CustomError si ya existe un camión con las mismas placas o usuario inválido.
+   */
+  static async create(data) {
+    const { user, truck, pickup, dropoff } = data;
+
+    const checkRelations = async (userId, truckId, originId, destinationId) => {
+      await Promise.all([
+        UsersController.getById(userId),
+        TrucksController.getById(truckId),
+        LocationsController.getById(originId),
+        LocationsController.getById(destinationId),
+      ]);
+    };
+    await checkRelations(user, truck, pickup, dropoff);
+    return OrderModel.create(data);
   }
 }
